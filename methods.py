4904511ee7c90d3ba48bbde911d50587f481bf97
maratgaliulin <egomarat@gmail.com>
2024-12-05 04:40:56 +1100

    minor changes

--- methods.py
+++ methods.py
@@ -18,9 +18,8 @@ def return_train_test_split(adult_df:pd.DataFrame):
     adult_df.dropna(inplace=True, ignore_index=True)
     adult_df['>50K,<=50K'].replace(['<=50K', '>50K'], [0, 1], inplace=True)
     adult_df['age_groups'] = adult_df.apply(replace_age_with_age_group, axis=1)
-    adult_df.drop(['fnlwgt', 'education-num'], inplace=True, axis=1)
     cols = [x for x in adult_df.columns if x != '>50K,<=50K']
-    cols_for_dummies = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'age_groups']
+    cols_for_dummies = ['workclass', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'age_groups']
     data = adult_df[cols]
     target = adult_df['>50K,<=50K']
     data = pd.get_dummies(data, columns=cols_for_dummies, dtype=int)
